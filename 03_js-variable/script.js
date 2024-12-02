1b1eba79322bc7ea5f3d1d8e88673f06115e64ee
Rashedul Hasan <84313852+rashedulhasanratul@users.noreply.github.com>
2024-12-03 01:12:34 +0600

    Add files via upload

---
+++ 03_js-variable/script.js
@@ -0,0 +1,36 @@
+// variable creating using let
+
+// let username = 'Hasan';
+// let userIntro = 'my name is ratul';
+let age = 25;
+let firstName = "Rashedul"
+let lastName = "hasan"
+let isHappy = true
+// let userIntro = 'Hi i am ' + username;
+ firstName = 'MD'
+ let a 
+
+a = 'Mahfuz'
+
+let userIntro = "Hi, my name is " + a + " " + lastName + '. I am '+ age +' years old.'
+
+
+// variable creating using const
+
+const hoursInDay = 24
+
+// hoursInDay = 12
+
+
+// variable creating using var
+
+var y = 'years'
+
+{var c ='code'}
+
+{let d = 'debug'}
+
+console.log(d)
+
+
+
---
+++ 05_js-dialog-box/script.js
@@ -0,0 +1,4 @@
+// const alertResult = alert("Your message goes here.")
+// const isConfirmed = confirm("Your message goes here.")
+const userInput = prompt("please enter your name")
+console.log(userInput)
