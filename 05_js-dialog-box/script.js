b114792ee9bbe3f591bda4c04f3819a70732fdd7
Rashedul Hasan <84313852+rashedulhasanratul@users.noreply.github.com>
2025-03-03 09:39:31 +0600

    Update script.js

--- 05_js-dialog-box/script.js
+++ 05_js-dialog-box/script.js
@@ -1,4 +1,13 @@
-// const alertResult = alert("Your message goes here.")
-// const isConfirmed = confirm("Your message goes here.")
-const userInput = prompt("please enter your name")
+//DIALOG BOX IN JS: alert, confirm and prompt.
+
+// alert("are you sure?");
+// confirm("Would you like to proceed?");
+// prompt("enter your name:");
+
+// store the result within the variable:
+
+// const alertResult = alert("Are you sure?");
+// const isConfirmed = confirm("Would you like proceed?.");
+const userInput = prompt("Please enter your name")
+
 console.log(userInput)
