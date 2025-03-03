f77d1d82b4f874f528bd409817d4ac39c2a5d5aa
Rashedul Hasan <84313852+rashedulhasanratul@users.noreply.github.com>
2025-03-03 09:11:25 +0600

    Update script.js

--- 03_js-variable/script.js
+++ 03_js-variable/script.js
@@ -1,18 +1,20 @@
 // variable creating using let
 
-// let username = 'Hasan';
-// let userIntro = 'my name is ratul';
-let age = 25;
+let username = 'Hasan';
+let age = 26;
+// let userIntro = 'I am' + ' ' + age + ' ' + "years old";
 let firstName = "Rashedul"
 let lastName = "hasan"
 let isHappy = true
-// let userIntro = 'Hi i am ' + username;
+
+// console.log(userIntro);
  firstName = 'MD'
  let a 
 
-a = 'Mahfuz'
+a = 'Abdulla'
 
-let userIntro = "Hi, my name is " + a + " " + lastName + '. I am '+ age +' years old.'
+let userIntro = "Hi, my name is " + a + " " + lastName + '. I am '+ age +' years old.';
+console.log(userIntro);
 
 
 // variable creating using const
@@ -22,15 +24,22 @@ const hoursInDay = 24
 // hoursInDay = 12
 
 
-// variable creating using var
+// variable creating using var:
 
 var y = 'years'
 
-{var c ='code'}
+{var c } // it can use outside the block because it is function scope
 
-{let d = 'debug'}
+{let d = 'debug'} // it cannot use outside of the block because it is  block scope
 
-console.log(d)
+// console.log(d)
 
 
+// this is also why we should avoid using var:
+// let r = 'tutul';
+// let r = "futul";
+// console.log(r);
 
+// var h='jack';
+// var h='jony'
+// console.log(h);
