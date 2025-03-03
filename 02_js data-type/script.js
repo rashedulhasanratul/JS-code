5093cfe5dd167fbec9c34840bcaeb7cd14924cc9
Rashedul Hasan <84313852+rashedulhasanratul@users.noreply.github.com>
2025-03-03 07:48:50 +0600

    Update script.js

--- 02_js data-type/script.js
+++ 02_js data-type/script.js
@@ -1,1 +1,14 @@
 console.log('hello');
+console.log(" Name: \'hasan\' ");
+console.log(' "Ratul" ');
+console.log(`hello, My name is "Rashedul hasan"`);
+
+console.log(1231545848747877454254877454847);
+console.log(1231545848747877454254877454847n);
+
+//creating bigInt Number:
+let bigNumber =121324574454765424534684754546474n;
+console.log(bigNumber);
+//another way  of creating;
+let bigN = BigInt("12315454121321848423123154231123584");
+console.log(bigN);
