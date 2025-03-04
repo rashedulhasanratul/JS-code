addc079e8967e8cb2aedb33fb89eaaf380cd4891
Rashedul Hasan <84313852+rashedulhasanratul@users.noreply.github.com>
2025-03-04 07:34:11 +0600

    ternary operator in js

---
+++ 15_js-ternary operator/script.js
@@ -0,0 +1,14 @@
+// --------Ternary Operator-------
+// debugger
+// const userName = 5 > 2 ? 'Hasan' : 'procoder';
+// console.log(userName);
+
+// debugger
+const gender = 'F'
+const userMessage =  `${gender.toLocaleLowerCase() === 'f' ? 'She' : 'He'} is a college student.`
+console.log(userMessage);
+
+//chain of ternary operator
+
+const result = null ? "hasan" : " " ? "12" : "0";
+console.log(result);
