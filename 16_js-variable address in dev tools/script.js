2c6027e0d64c2ed09518f86a3468ec4bbe89d9b5
Rashedul Hasan <84313852+rashedulhasanratul@users.noreply.github.com>
2025-03-04 07:58:29 +0600

    learning variable address using dev tools

---
+++ 16_js-variable address in dev tools/script.js
@@ -0,0 +1,12 @@
+//  Variable address in  DEV TOOLS. 
+
+const firstName = 'Akash';
+const firstName2 = 'Aka' + 'sh';
+const LastName= 'Singh';
+const userAge = 15;
+const birthYear = 2000;
+const isGraduate = false;
+const hasJob = false;
+const myNull = null;
+const myUndefined = undefined;
+const myEmptyString = '';
