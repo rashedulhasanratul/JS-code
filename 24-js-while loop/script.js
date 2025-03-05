d73940877c54f252493e82f9164d1a014fb67876
Rashedul Hasan <84313852+rashedulhasanratul@users.noreply.github.com>
2025-03-05 15:03:05 +0600

    while loop

---
+++ 24-js-while loop/script.js
@@ -0,0 +1,18 @@
+// while loop:
+
+// debugger
+
+let num=1;
+while(num<=100){
+    console.log(num);
+    num++;
+}
+
+const friends =['Hasan','Ratul','Sajjad','Rana','Piyas','anurag','akash']
+// console.log(friends);
+
+let i=0;
+while(i<friends.length){
+    console.log(`${i+1}.`,`${friends[i]} procoder`);
+    i++;
+}
