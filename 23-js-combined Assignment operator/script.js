6d67577af2608d5ed7fc19ae2cf052961b57347d
Rashedul Hasan <84313852+rashedulhasanratul@users.noreply.github.com>
2025-03-05 14:16:25 +0600

    compound operator

---
+++ 23-js-combined Assignment operator/script.js
@@ -0,0 +1,27 @@
+// TOPIC: combined assignment operator
+
+
+let num = 5;
+num = num + 2;
+
+num +=5;
+num -=2;
+num *=3;
+num %=2;
+num /=2;
+num **=3;
+
+//increment operator  and decrement operator:
+
+// let newNum = num++;
+// let v = newNum++
+// console.log(v);
+let newNum = ++num;
+let v = newNum++
+console.log(newNum);
+
+let dumb = 7
+
+let h = dumb--;
+
+// let h = --dumb
