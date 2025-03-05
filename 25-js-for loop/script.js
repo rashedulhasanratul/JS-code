69813b632587943b4fe4c91a1deb898b5810d535
Rashedul Hasan <84313852+rashedulhasanratul@users.noreply.github.com>
2025-03-05 15:20:36 +0600

    for loop

---
+++ 25-js-for loop/script.js
@@ -0,0 +1,23 @@
+// for loop:
+
+// debugger
+
+for(let n=0; n<10; n++){
+    console.log(n+1,'hasan');
+}
+
+const friends =['Hasan','Ratul','Sajjad','Rana','Piyas','anurag','akash']
+// // console.log(friends);
+
+for(let i=0; i<friends.length; i++){
+    console.log(`${i+1}.`,friends[i]);
+}
+
+//even number: 0-100
+
+for(let i=0; i<=100;i++){
+    
+    if(i%2==0){
+        console.log(i);
+    }
+}
