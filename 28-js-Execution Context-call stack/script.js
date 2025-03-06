fb74cf518e8d6a4b0b678f15fb66e33d2bdaa097
Rashedul Hasan <84313852+rashedulhasanratul@users.noreply.github.com>
2025-03-06 08:53:51 +0600

    details about execution context and call stack

---
+++ 28-js-Execution Context-call stack/script.js
@@ -0,0 +1,34 @@
+debugger
+
+// sayHello();
+
+// const userName = 'Hasan';
+// const userAge = 26;
+// // console.log(userName);
+
+
+
+// function sayHello(){
+//     const a = 12
+//     const b = 3
+//     addTwoNumber(4, 5)
+// }
+
+// function addTwoNumber(x, y){
+//     nothing()
+//     return x + y
+// }
+
+// function nothing(){
+//     console.log(`hi, it's nothing`);
+// }
+
+function introduceMe(){
+    console.log("my name is Hasan");
+    introduceMe()
+}
+
+introduceMe()
+
+
+console.log("program Ended");
