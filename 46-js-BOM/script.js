48be2f11d26ffb8726c5445431c99e4a621d3fd6
Rashedul Hasan <84313852+rashedulhasanratul@users.noreply.github.com>
2025-03-15 16:41:30 +0600

    browser object model concept

---
+++ 46-js-BOM/script.js
@@ -0,0 +1,15 @@
+// Bom in js: basically it interact with the browser and control browsers behavior. root object: window
+
+/* Bom defines Browser Object Model that refers to the set of object that allow interaction with
+the browser itself. It enable developers to control browser functionality likes navigation, alert
+and screen size.
+*/
+
+
+// location.reload()
+// location.href= 'https://developer.mozilla.org/en-US/'
+// element.scroll({
+//     top: 100,
+//     left: 100,
+//     behavior: "smooth",
+//   });
