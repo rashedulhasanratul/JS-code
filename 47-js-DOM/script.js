1825e05146e1273658ce5811a0499aa922e701a2
Rashedul Hasan <84313852+rashedulhasanratul@users.noreply.github.com>
2025-03-15 16:41:51 +0600

    Dom concept

---
+++ 47-js-DOM/script.js
@@ -0,0 +1,12 @@
+//  DOM basic details : Document Object Model
+// dom manipulation: is when we use js to add, remove, and modify elements of a website.
+
+function sayHi() {
+    document.body.children[4].src =
+        "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?cs=srgb&dl=pexels-souvenirpixels-414612.jpg&fm=jpg";
+}
+
+function sayCSS(){
+    document.body.children[5].children[5].src = "https://thumbs.dreamstime.com/b/autumn-nature-landscape-colorful-forest-autumn-nature-landscape-colorful-forest-morning-sunlight-131400332.jpg"
+
+}
